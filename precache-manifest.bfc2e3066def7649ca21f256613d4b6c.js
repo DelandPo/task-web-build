cc683e0cf51b5c68c9ea39d8aa489baf0fec29cc
Ananda Poudel <apoudel@stcloudstate.edu>
2018-11-27 13:15:19 -0600

    First build

---
+++ precache-manifest.bfc2e3066def7649ca21f256613d4b6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "be99606d37957baeacfb",
+    "url": "/static/js/main.be99606d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "be99606d37957baeacfb",
+    "url": "/static/css/main.dc010248.chunk.css"
+  },
+  {
+    "revision": "a153cc6d041a7c2680f4ea0c630f48ab",
+    "url": "/index.html"
+  }
+];
